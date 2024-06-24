cfcd058adfaf799b77e49f4f3e911638623ccf2a
Parami <paramiashinsana@gmail.com>
2024-06-24 20:36:15 +0530

    Add @EnableConfigServer in ConfigurationServiceApplication

--- ConfigurationService/src/main/java/org/example2/configurationservice/ConfigurationServiceApplication.java
+++ ConfigurationService/src/main/java/org/example2/configurationservice/ConfigurationServiceApplication.java
@@ -3,8 +3,10 @@ package org.example2.configurationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
 public class ConfigurationServiceApplication {
 
