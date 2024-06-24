806be7cd73825b8a2deb8e81da6901d7a3fc2035
Parami <paramiashinsana@gmail.com>
2024-06-24 22:03:02 +0530

    Update the PaymentServiceApplication

--- PaymentService/src/main/java/org/example2/paymentservice/PaymentServiceApplication.java
+++ PaymentService/src/main/java/org/example2/paymentservice/PaymentServiceApplication.java
@@ -3,6 +3,8 @@ package org.example2.paymentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +14,8 @@ public class PaymentServiceApplication {
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
