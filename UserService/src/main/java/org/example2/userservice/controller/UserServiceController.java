fa0091eff9fdb31d8b2a2b23d1167717263f8c82
Parami <paramiashinsana@gmail.com>
2024-06-25 00:20:44 +0530

    Update the UserServiceController

--- UserService/src/main/java/org/example2/userservice/controller/UserServiceController.java
+++ UserService/src/main/java/org/example2/userservice/controller/UserServiceController.java
@@ -1,4 +1,9 @@
 package org.example2.userservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserServiceController {
 }
