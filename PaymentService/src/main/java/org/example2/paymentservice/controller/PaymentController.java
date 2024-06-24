8cd9167437ef86823b71677792dcb1a0fea17ca8
Parami <paramiashinsana@gmail.com>
2024-06-24 22:04:42 +0530

    Update the PaymentController

--- PaymentService/src/main/java/org/example2/paymentservice/controller/PaymentController.java
+++ PaymentService/src/main/java/org/example2/paymentservice/controller/PaymentController.java
@@ -1,4 +1,9 @@
 package org.example2.paymentservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/payment")
 public class PaymentController {
 }
