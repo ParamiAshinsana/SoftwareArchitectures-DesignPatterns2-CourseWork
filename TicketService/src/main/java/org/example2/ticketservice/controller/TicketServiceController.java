52bd59a30a5aa5c65d923fc686e1680c264b9ea1
Parami <paramiashinsana@gmail.com>
2024-06-25 00:00:22 +0530

    Update the TicketServiceController

--- TicketService/src/main/java/org/example2/ticketservice/controller/TicketServiceController.java
+++ TicketService/src/main/java/org/example2/ticketservice/controller/TicketServiceController.java
@@ -1,4 +1,9 @@
 package org.example2.ticketservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/ticket")
 public class TicketServiceController {
 }
