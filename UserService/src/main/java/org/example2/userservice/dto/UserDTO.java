63888e53d0eb51a62a979f5b04846422f0780d7f
Parami <paramiashinsana@gmail.com>
2024-06-25 16:38:06 +0530

    Update UserDTO

--- UserService/src/main/java/org/example2/userservice/dto/UserDTO.java
+++ UserService/src/main/java/org/example2/userservice/dto/UserDTO.java
@@ -1,4 +1,19 @@
 package org.example2.userservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserDTO {
+    private String userId;
+    private String userName;
+    private String userDOB;
+    private String userGender;
+    private String userAddress;
+    private String userContact;
 }
