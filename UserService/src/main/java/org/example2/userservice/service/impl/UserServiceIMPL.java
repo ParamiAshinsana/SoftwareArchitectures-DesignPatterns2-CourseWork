ec1a896dae74616278942dee3aa861e1262fd123
Parami <paramiashinsana@gmail.com>
2024-06-26 22:51:21 +0530

    Update the UserServiceIMPL

--- UserService/src/main/java/org/example2/userservice/service/impl/UserServiceIMPL.java
+++ UserService/src/main/java/org/example2/userservice/service/impl/UserServiceIMPL.java
@@ -1,39 +1,44 @@
-//package org.example2.userservice.service.impl;
-//
-//import jakarta.transaction.Transactional;
-//import lombok.RequiredArgsConstructor;
-//import org.example2.userservice.dto.UserDTO;
-//import org.example2.userservice.service.UserService;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class UserServiceIMPL implements UserService {
-//    @Override
-//    public UserDTO saveUser(UserDTO userDTO) {
-//        return null;
-//    }
-//
-//    @Override
-//    public void deleteUser(String id) {
-//
-//    }
-//
-//    @Override
-//    public void updateUser(String id, UserDTO userDTO) {
-//
-//    }
-//
-//    @Override
-//    public List<UserDTO> getAllUsers() {
-//        return null;
-//    }
-//
-//    @Override
-//    public UserDTO getSelectedUser(String id) {
-//        return null;
-//    }
-//}
+package org.example2.userservice.service.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.example2.userservice.dto.UserDTO;
+import org.example2.userservice.repository.UserDAO;
+import org.example2.userservice.service.UserService;
+import org.example2.userservice.util.UserMapping;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserServiceIMPL implements UserService {
+    private final UserDAO userDAO;
+    private final UserMapping userMapping;
+
+    @Override
+    public UserDTO saveUser(UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String id) {
+
+    }
+
+    @Override
+    public void updateUser(String id, UserDTO userDTO) {
+
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public UserDTO getSelectedUser(String id) {
+        return null;
+    }
+}
