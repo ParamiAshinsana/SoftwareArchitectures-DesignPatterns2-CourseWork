1baf10b369007337d005903f55e73ac6049dde0e
Parami <paramiashinsana@gmail.com>
2024-06-26 22:53:14 +0530

    Update the UserEntity

--- UserService/src/main/java/org/example2/userservice/entity/UserEntity.java
+++ UserService/src/main/java/org/example2/userservice/entity/UserEntity.java
@@ -1,25 +1,21 @@
-//package org.example2.userservice.entity;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@Entity
-//@Table(name = "user")
-//public class UserEntity {
-//    @Id
-//    private String userId;
-//    private String userName;
-//    private String userDOB;
-//    private String userGender;
-//    private String userAddress;
-//    private String userContact;
-//}
+package org.example2.userservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class UserEntity {
+    @Id
+    private String userId;
+    private String userName;
+    private String userDOB;
+    private String userGender;
+    private String userAddress;
+    private String userContact;
+}
