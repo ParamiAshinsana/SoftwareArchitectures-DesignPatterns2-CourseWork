01d7679fd0460c14b1915c343ccc40f00d5da786
Parami <paramiashinsana@gmail.com>
2024-06-26 21:11:21 +0530

    Update

--- UserService/src/main/java/org/example2/userservice/service/impl/UserServiceIMPL.java
+++ UserService/src/main/java/org/example2/userservice/service/impl/UserServiceIMPL.java
@@ -1,39 +1,39 @@
-package org.example2.userservice.service.impl;
-
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.example2.userservice.dto.UserDTO;
-import org.example2.userservice.service.UserService;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class UserServiceIMPL implements UserService {
-    @Override
-    public UserDTO saveUser(UserDTO userDTO) {
-        return null;
-    }
-
-    @Override
-    public void deleteUser(String id) {
-
-    }
-
-    @Override
-    public void updateUser(String id, UserDTO userDTO) {
-
-    }
-
-    @Override
-    public List<UserDTO> getAllUsers() {
-        return null;
-    }
-
-    @Override
-    public UserDTO getSelectedUser(String id) {
-        return null;
-    }
-}
+//package org.example2.userservice.service.impl;
+//
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import org.example2.userservice.dto.UserDTO;
+//import org.example2.userservice.service.UserService;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@Transactional
+//@RequiredArgsConstructor
+//public class UserServiceIMPL implements UserService {
+//    @Override
+//    public UserDTO saveUser(UserDTO userDTO) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void deleteUser(String id) {
+//
+//    }
+//
+//    @Override
+//    public void updateUser(String id, UserDTO userDTO) {
+//
+//    }
+//
+//    @Override
+//    public List<UserDTO> getAllUsers() {
+//        return null;
+//    }
+//
+//    @Override
+//    public UserDTO getSelectedUser(String id) {
+//        return null;
+//    }
+//}
