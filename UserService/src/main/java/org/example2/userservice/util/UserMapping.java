91289e9def3802c4b85ac8acf9cc620873004250
Parami <paramiashinsana@gmail.com>
2024-06-26 22:53:02 +0530

    Update the UserMapping

--- UserService/src/main/java/org/example2/userservice/util/UserMapping.java
+++ UserService/src/main/java/org/example2/userservice/util/UserMapping.java
@@ -1,4 +1,13 @@
-//package org.example2.userservice.util;
-//
-//public class UserMapping {
-//}
+package org.example2.userservice.util;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserMapping {
+     private final ModelMapper modelMapper;
+
+
+}
