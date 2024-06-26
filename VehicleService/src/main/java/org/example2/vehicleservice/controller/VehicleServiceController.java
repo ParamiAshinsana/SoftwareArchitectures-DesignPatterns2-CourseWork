ce449dc018288173f85ce663c8e193a63ea44e6a
Parami <paramiashinsana@gmail.com>
2024-06-26 21:09:23 +0530

    Update

--- VehicleService/src/main/java/org/example2/vehicleservice/controller/VehicleServiceController.java
+++ VehicleService/src/main/java/org/example2/vehicleservice/controller/VehicleServiceController.java
@@ -1,9 +1,16 @@
 package org.example2.vehicleservice.controller;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/vehicle")
 public class VehicleServiceController {
+
+    @GetMapping("/userService")
+    public String getDetail(){
+        return "Hello";
+    }
 }
