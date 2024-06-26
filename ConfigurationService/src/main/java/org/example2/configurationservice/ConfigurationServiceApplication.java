6b81be15665322cf860642e6e0840e5a31bbd8ed
Parami <paramiashinsana@gmail.com>
2024-06-26 21:17:08 +0530

    Update

--- ConfigurationService/src/main/java/org/example2/configurationservice/ConfigurationServiceApplication.java
+++ ConfigurationService/src/main/java/org/example2/configurationservice/ConfigurationServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
+
 public class ConfigurationServiceApplication {
 
     public static void main(String[] args) {
