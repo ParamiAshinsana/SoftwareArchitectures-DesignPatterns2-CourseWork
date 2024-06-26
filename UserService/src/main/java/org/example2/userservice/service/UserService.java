a7c247dbf1ae31dc23b673554a0cdc911d8bd6e5
Parami <paramiashinsana@gmail.com>
2024-06-26 22:52:47 +0530

    Update the UserService

--- UserService/src/main/java/org/example2/userservice/service/UserService.java
+++ UserService/src/main/java/org/example2/userservice/service/UserService.java
@@ -1,13 +1,13 @@
-//package org.example2.userservice.service;
-//
-//import org.example2.userservice.dto.UserDTO;
-//
-//import java.util.List;
-//
-//public interface UserService {
-//    UserDTO saveUser(UserDTO userDTO);
-//    void deleteUser(String id);
-//    void updateUser(String id, UserDTO userDTO);
-//    List<UserDTO> getAllUsers();
-//    UserDTO getSelectedUser(String id);
-//}
+package org.example2.userservice.service;
+
+import org.example2.userservice.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+    UserDTO saveUser(UserDTO userDTO);
+    void deleteUser(String id);
+    void updateUser(String id, UserDTO userDTO);
+    List<UserDTO> getAllUsers();
+    UserDTO getSelectedUser(String id);
+}
