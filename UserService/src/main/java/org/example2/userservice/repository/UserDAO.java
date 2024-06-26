cc98a9b7bf757bc38166e21b627652d3f0087ae2
Parami <paramiashinsana@gmail.com>
2024-06-26 22:53:43 +0530

    Update the UserDAO

--- UserService/src/main/java/org/example2/userservice/repository/UserDAO.java
+++ UserService/src/main/java/org/example2/userservice/repository/UserDAO.java
@@ -1,9 +1,9 @@
-//package org.example2.userservice.repository;
-//
-//import org.example2.userservice.entity.UserEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UserDAO extends JpaRepository<UserEntity, String> {
-//}
+package org.example2.userservice.repository;
+
+import org.example2.userservice.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDAO extends JpaRepository<UserEntity, String> {
+}
