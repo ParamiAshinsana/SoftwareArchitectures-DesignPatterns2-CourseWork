2aeb3d6f211e967fb624fc9cb5d8746c474fda0a
Parami <paramiashinsana@gmail.com>
2024-06-26 22:52:06 +0530

    Update the UserServiceController

--- UserService/src/main/java/org/example2/userservice/controller/UserServiceController.java
+++ UserService/src/main/java/org/example2/userservice/controller/UserServiceController.java
@@ -1,12 +1,22 @@
 package org.example2.userservice.controller;
+import lombok.RequiredArgsConstructor;
+import org.example2.userservice.dto.UserDTO;
+import org.example2.userservice.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 public class UserServiceController {
+    private final UserService userService;
 
     @GetMapping("/userService")
     public String getDetail(){
         return "Hello";
+    }
+
+    @PostMapping(value = "/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
     }
 }
