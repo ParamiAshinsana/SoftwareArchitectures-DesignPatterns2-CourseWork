237e6d33158f94bad881509aba61d3b12d73187d
Parami <paramiashinsana@gmail.com>
2024-06-28 22:54:43 +0530

    Update VehicleServiceDTO

---
+++ VehicleService/src/main/java/org/example2/vehicleservice/dto/VehicleServiceDTO.java
@@ -0,0 +1,17 @@
+package org.example2.vehicleservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class VehicleServiceDTO {
+    private String vehicleRegistrationId;
+    private String vehicleType;
+    private String fuelType;
+    private String vehicleNo;
+    private String nameOfOwner;
+    private String addressOfOwner;
+}
