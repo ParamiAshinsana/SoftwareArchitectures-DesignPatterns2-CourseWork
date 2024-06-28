0bee236729ae33edd8ae6e97464dda48b5f30b24
Parami <paramiashinsana@gmail.com>
2024-06-28 23:30:08 +0530

    Update VehicleServiceIMPL

--- VehicleService/src/main/java/org/example2/vehicleservice/service/impl/VehicleServiceIMPL.java
+++ VehicleService/src/main/java/org/example2/vehicleservice/service/impl/VehicleServiceIMPL.java
@@ -1,4 +1,33 @@
 package org.example2.vehicleservice.service.impl;
 
-public class VehicleServiceIMPL {
+import org.example2.vehicleservice.dto.VehicleDTO;
+import org.example2.vehicleservice.service.VehicleService;
+
+import java.util.List;
+
+public class VehicleServiceIMPL implements VehicleService {
+    @Override
+    public VehicleDTO saveVehicle(VehicleDTO vehicleDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteVehicle(String id) {
+
+    }
+
+    @Override
+    public void updateVehicle(String id, VehicleDTO vehicleDTO) {
+
+    }
+
+    @Override
+    public List<VehicleDTO> getAllVehicles() {
+        return null;
+    }
+
+    @Override
+    public VehicleDTO getSelectedVehicle(String id) {
+        return null;
+    }
 }
