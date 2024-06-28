4215af09bf6990381a4c78dfd42f8891db2259cb
Parami <paramiashinsana@gmail.com>
2024-06-28 23:27:41 +0530

    Update VehicleService

--- VehicleService/src/main/java/org/example2/vehicleservice/service/VehicleService.java
+++ VehicleService/src/main/java/org/example2/vehicleservice/service/VehicleService.java
@@ -1,4 +1,13 @@
 package org.example2.vehicleservice.service;
 
+import org.example2.vehicleservice.dto.VehicleDTO;
+
+import java.util.List;
+
 public interface VehicleService {
+    VehicleDTO saveVehicle(VehicleDTO vehicleDTO);
+    void deleteVehicle(String id);
+    void updateVehicle(String id, VehicleDTO vehicleDTO);
+    List<VehicleDTO> getAllVehicles();
+    VehicleDTO getSelectedVehicle(String id);
 }
