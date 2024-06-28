9585778e43eaed8950cfdd21eafa971fcc3d3db7
Parami <paramiashinsana@gmail.com>
2024-06-28 23:20:19 +0530

    Update VehicleServiceEntity

--- VehicleService/src/main/java/org/example2/vehicleservice/entity/VehicleServiceEntity.java
+++ VehicleService/src/main/java/org/example2/vehicleservice/entity/VehicleServiceEntity.java
@@ -1,4 +1,23 @@
 package org.example2.vehicleservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "vehicle")
 public class VehicleServiceEntity {
+    @Id
+    private String vehicleRegistrationId;
+    private String vehicleType;
+    private String fuelType;
+    private String vehicleNo;
+    private String nameOfOwner;
+    private String addressOfOwner;
 }
