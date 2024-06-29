ce2894a4da1fb1a7cf0547e83af6f562b1768d9a
Parami <paramiashinsana@gmail.com>
2024-06-29 23:08:35 +0530

    Update TicketDTO

--- TicketService/src/main/java/org/example2/ticketservice/dto/TicketDTO.java
+++ TicketService/src/main/java/org/example2/ticketservice/dto/TicketDTO.java
@@ -8,4 +8,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class TicketDTO {
+    private String tellerId;
+    private String issuedDate;
+    private String issuedTime;
+    private String entranceIC;
+    private String exitIC;
+    private int vehicleType;
+    private String vehicleNo;
+    private String averageSpeed;
+    private String travelTime;
+    private double amount;
+    private String paymentStatus;
 }
