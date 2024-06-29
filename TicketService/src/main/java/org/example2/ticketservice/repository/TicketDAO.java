938a559ea7cc637c33bb0f8f42ca6d5e3755d63a
Parami <paramiashinsana@gmail.com>
2024-06-29 22:41:20 +0530

    Update TicketDAO

--- TicketService/src/main/java/org/example2/ticketservice/repository/TicketDAO.java
+++ TicketService/src/main/java/org/example2/ticketservice/repository/TicketDAO.java
@@ -1,4 +1,9 @@
 package org.example2.ticketservice.repository;
 
-public interface TicketDAO {
+import org.example2.ticketservice.entity.TicketEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketDAO extends JpaRepository<TicketEntity, String> {
 }
