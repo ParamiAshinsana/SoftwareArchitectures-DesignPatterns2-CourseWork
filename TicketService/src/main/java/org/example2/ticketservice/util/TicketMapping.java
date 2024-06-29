4ea422411325d755181747ae7b42a0fdf25cedde
Parami <paramiashinsana@gmail.com>
2024-06-29 22:46:31 +0530

    Update TicketMapping

--- TicketService/src/main/java/org/example2/ticketservice/util/TicketMapping.java
+++ TicketService/src/main/java/org/example2/ticketservice/util/TicketMapping.java
@@ -1,9 +1,25 @@
 package org.example2.ticketservice.util;
 
 import lombok.RequiredArgsConstructor;
+import org.example2.ticketservice.dto.TicketDTO;
+import org.example2.ticketservice.entity.TicketEntity;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class TicketMapping {
+    private final ModelMapper modelMapper;
+
+    public TicketDTO toTicketDTO(TicketEntity ticketEntity) {
+        return  modelMapper.map(ticketEntity, TicketDTO.class);
+    }
+    public TicketEntity toTicket(TicketDTO userDTO) {
+        return  modelMapper.map(userDTO, TicketEntity.class);
+    }
+    public List<TicketDTO> toTicketDTOList(List<TicketEntity> ticketEntities) {
+        return modelMapper.map(ticketEntities, List.class);
+    }
 }
