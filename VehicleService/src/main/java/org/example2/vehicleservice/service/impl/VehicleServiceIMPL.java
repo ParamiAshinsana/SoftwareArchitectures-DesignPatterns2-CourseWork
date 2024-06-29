45519ff414540cf3a103d99d66e20dcf7d28401a
Parami <paramiashinsana@gmail.com>
2024-06-29 15:10:37 +0530

    Update VehicleServiceIMPL

--- VehicleService/src/main/java/org/example2/vehicleservice/service/impl/VehicleServiceIMPL.java
+++ VehicleService/src/main/java/org/example2/vehicleservice/service/impl/VehicleServiceIMPL.java
@@ -1,14 +1,27 @@
 package org.example2.vehicleservice.service.impl;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.example2.vehicleservice.dto.VehicleDTO;
+import org.example2.vehicleservice.repository.VehicleDAO;
 import org.example2.vehicleservice.service.VehicleService;
+import org.example2.vehicleservice.util.VehicleMapping;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class VehicleServiceIMPL implements VehicleService {
+    private final VehicleMapping vehicleMapping;
+    private final VehicleDAO vehicleDAO;
+
     @Override
     public VehicleDTO saveVehicle(VehicleDTO vehicleDTO) {
-        return null;
+        vehicleDTO.setVehicleRegistrationId(UUID.randomUUID().toString());
+         return vehicleMapping.toVehicleDTO(vehicleDAO.save(vehicleMapping.toVehicle(vehicleDTO)));
     }
 
     @Override
