424922ca9c36680e16fe29e19877a478ae9f2744
Parami <paramiashinsana@gmail.com>
2024-06-29 22:40:34 +0530

    Update TicketEntity

--- TicketService/src/main/java/org/example2/ticketservice/entity/TicketEntity.java
+++ TicketService/src/main/java/org/example2/ticketservice/entity/TicketEntity.java
@@ -1,4 +1,17 @@
 package org.example2.ticketservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "ticket")
 public class TicketEntity {
+    @Id
 }
