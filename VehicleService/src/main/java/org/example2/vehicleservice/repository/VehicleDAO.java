d1c56fca6e8b2d21592ea9a90f82e201cd46998a
Parami <paramiashinsana@gmail.com>
2024-06-29 20:04:06 +0530

    Update VehicleDAO

--- VehicleService/src/main/java/org/example2/vehicleservice/repository/VehicleDAO.java
+++ VehicleService/src/main/java/org/example2/vehicleservice/repository/VehicleDAO.java
@@ -4,6 +4,11 @@ import org.example2.vehicleservice.entity.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VehicleDAO extends JpaRepository<VehicleEntity, String> {
+    Optional<VehicleEntity> findByVehicleTypeAndVehicleNo(String vehicleType, String vehicleNo);
+
+    boolean existsByVehicleTypeAndVehicleNo(String vehicleType, String vehicleNo);
 }
