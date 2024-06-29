e46b482ebcd03eca650def9a252bd4e919fe44e0
Parami <paramiashinsana@gmail.com>
2024-06-29 22:39:31 +0530

    Update TicketDTO

--- TicketService/src/main/java/org/example2/ticketservice/dto/TicketDTO.java
+++ TicketService/src/main/java/org/example2/ticketservice/dto/TicketDTO.java
@@ -1,4 +1,11 @@
 package org.example2.ticketservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TicketDTO {
 }
