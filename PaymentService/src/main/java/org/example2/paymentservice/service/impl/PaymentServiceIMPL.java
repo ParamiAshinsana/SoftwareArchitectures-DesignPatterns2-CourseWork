3d842a7db574521de6fadb7e936f55fa4f9da58d
Parami <paramiashinsana@gmail.com>
2024-07-01 16:22:06 +0530

    Update PaymentServiceIMPL

--- PaymentService/src/main/java/org/example2/paymentservice/service/impl/PaymentServiceIMPL.java
+++ PaymentService/src/main/java/org/example2/paymentservice/service/impl/PaymentServiceIMPL.java
@@ -1,4 +1,11 @@
 package org.example2.paymentservice.service.impl;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class PaymentServiceIMPL {
 }
