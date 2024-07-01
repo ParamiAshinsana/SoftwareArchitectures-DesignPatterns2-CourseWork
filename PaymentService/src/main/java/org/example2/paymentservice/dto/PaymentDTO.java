131c7e32d21ff0cc6ae83cafb6ab4a8ebda345de
Parami <paramiashinsana@gmail.com>
2024-07-01 16:21:15 +0530

    Update PaymentDTO

--- PaymentService/src/main/java/org/example2/paymentservice/dto/PaymentDTO.java
+++ PaymentService/src/main/java/org/example2/paymentservice/dto/PaymentDTO.java
@@ -1,4 +1,11 @@
 package org.example2.paymentservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class PaymentDTO {
 }
