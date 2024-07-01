eb3dd55406feed7077fe9afafe041fff08504fdb
Parami <paramiashinsana@gmail.com>
2024-07-01 18:20:35 +0530

    Update PaymentServiceApplication

--- PaymentService/src/main/java/org/example2/paymentservice/controller/PaymentController.java
+++ PaymentService/src/main/java/org/example2/paymentservice/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package org.example2.paymentservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/payment")
 public class PaymentController {
+    @GetMapping("/paymentService")
+    public String getDetail(){
+        return "Hello Payment";
+    }
 }
