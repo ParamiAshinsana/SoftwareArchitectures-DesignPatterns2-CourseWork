d5401c951297606fdda1809fbec4a2506a7f33b8
Parami <paramiashinsana@gmail.com>
2024-07-01 18:20:22 +0530

    Update TicketServiceApplication

--- TicketService/src/main/java/org/example2/ticketservice/controller/TicketServiceController.java
+++ TicketService/src/main/java/org/example2/ticketservice/controller/TicketServiceController.java
@@ -1,6 +1,7 @@
 package org.example2.ticketservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/ticket")
 public class TicketServiceController {
+    @GetMapping("/ticketService")
+    public String getDetail(){
+        return "Hello Tickets";
+    }
 }
