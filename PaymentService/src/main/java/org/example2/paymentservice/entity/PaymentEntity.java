45c413d83c5bc88f8d1b47aabb1fe5d401b7d4dc
Parami <paramiashinsana@gmail.com>
2024-07-01 16:21:31 +0530

    Update PaymentEntity

--- PaymentService/src/main/java/org/example2/paymentservice/entity/PaymentEntity.java
+++ PaymentService/src/main/java/org/example2/paymentservice/entity/PaymentEntity.java
@@ -1,4 +1,17 @@
 package org.example2.paymentservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "payment")
 public class PaymentEntity {
+    @Id
 }
