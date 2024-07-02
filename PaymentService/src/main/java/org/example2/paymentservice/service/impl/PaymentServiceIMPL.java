be6e3d32b9e1c97746a57aa1c1cb4fb38bad9d4e
Parami <paramiashinsana@gmail.com>
2024-07-03 00:16:40 +0530

    Update PaymentServiceIMPL

--- PaymentService/src/main/java/org/example2/paymentservice/service/impl/PaymentServiceIMPL.java
+++ PaymentService/src/main/java/org/example2/paymentservice/service/impl/PaymentServiceIMPL.java
@@ -2,10 +2,38 @@ package org.example2.paymentservice.service.impl;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.example2.paymentservice.dto.PaymentDTO;
+import org.example2.paymentservice.service.PaymentService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class PaymentServiceIMPL {
+public class PaymentServiceIMPL implements PaymentService {
+    @Override
+    public PaymentDTO processThePayment(PaymentDTO paymentDTO) {
+        return null;
+    }
+
+    @Override
+    public void deletePayment(String id) {
+
+    }
+
+    @Override
+    public void modifyThePayment(String id, PaymentDTO paymentDTO) {
+
+    }
+
+    @Override
+    public List<PaymentDTO> getAllPaymentDetails() {
+        return null;
+    }
+
+    @Override
+    public PaymentDTO getSelectedPaymentDetails(String id) {
+        return null;
+    }
 }
