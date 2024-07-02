bede3e32e077ca7b36cd93a19c0400f1defe1bb5
Parami <paramiashinsana@gmail.com>
2024-07-03 00:16:24 +0530

    Update PaymentService

--- PaymentService/src/main/java/org/example2/paymentservice/service/PaymentService.java
+++ PaymentService/src/main/java/org/example2/paymentservice/service/PaymentService.java
@@ -1,4 +1,13 @@
 package org.example2.paymentservice.service;
 
+import org.example2.paymentservice.dto.PaymentDTO;
+
+import java.util.List;
+
 public interface PaymentService {
+    PaymentDTO processThePayment(PaymentDTO paymentDTO);
+    void deletePayment(String id);
+    void modifyThePayment(String id, PaymentDTO paymentDTO);
+    List<PaymentDTO> getAllPaymentDetails();
+    PaymentDTO getSelectedPaymentDetails(String id);
 }
