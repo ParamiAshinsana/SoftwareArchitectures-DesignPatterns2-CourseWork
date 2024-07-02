509b8cd3eeb827a14dc6d32e4ba6066ac077b67b
Parami <paramiashinsana@gmail.com>
2024-07-03 01:14:50 +0530

    Update PaymentController

--- PaymentService/src/main/java/org/example2/paymentservice/controller/PaymentController.java
+++ PaymentService/src/main/java/org/example2/paymentservice/controller/PaymentController.java
@@ -1,16 +1,27 @@
 package org.example2.paymentservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example2.paymentservice.dto.PaymentDTO;
+import org.example2.paymentservice.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/payment")
 public class PaymentController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final PaymentService paymentService;
     @GetMapping("/paymentService")
     public String getDetail(){
         return "Hello";
+    }
+
+    @PostMapping(value = "/processThePayment")
+    public PaymentDTO processThePayment(@RequestBody PaymentDTO paymentDTO){
+        return paymentService.processThePayment(paymentDTO);
     }
 }
