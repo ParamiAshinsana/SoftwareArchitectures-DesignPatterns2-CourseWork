9548eeecc1b222f0bb119583761fe2903b4ba0b6
Parami <paramiashinsana@gmail.com>
2024-07-02 16:59:26 +0530

    Update TicketEntity

--- TicketService/src/main/java/org/example2/ticketservice/entity/TicketEntity.java
+++ TicketService/src/main/java/org/example2/ticketservice/entity/TicketEntity.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example2.ticketservice.enumeration.PaymentStatus;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -14,8 +17,8 @@ import org.example2.ticketservice.enumeration.PaymentStatus;
 public class TicketEntity {
     @Id
     private String tellerId;
-    private String issuedDate;
-    private String issuedTime;
+    private LocalDate issuedDate;
+    private LocalTime issuedTime;
     private String entranceIC;
     private String exitIC;
     private int vehicleType;
