2d4a63671585ea6a8a3bb2402340149bb4aa17e1
Parami <paramiashinsana@gmail.com>
2024-07-02 16:59:44 +0530

    Update TicketServiceIMPL

--- TicketService/src/main/java/org/example2/ticketservice/service/impl/TicketServiceIMPL.java
+++ TicketService/src/main/java/org/example2/ticketservice/service/impl/TicketServiceIMPL.java
@@ -3,7 +3,10 @@ package org.example2.ticketservice.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example2.ticketservice.dto.TicketDTO;
+import org.example2.ticketservice.entity.TicketEntity;
+import org.example2.ticketservice.repository.TicketDAO;
 import org.example2.ticketservice.service.TicketService;
+import org.example2.ticketservice.util.TicketMapping;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,9 +15,15 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class TicketServiceIMPL implements TicketService {
+    private final TicketDAO ticketDAO;
+    private final TicketMapping ticketMapping;
+
     @Override
     public TicketDTO issueTicketAtEntrance(TicketDTO ticketDTO) {
-        return null;
+        TicketEntity ticketEntity = ticketMapping.toTicket(ticketDTO);
+
+        ticketEntity = ticketDAO.save(ticketEntity);
+        return ticketMapping.toTicketDTO(ticketEntity);
     }
 
     @Override
