65c103f8824ba60f044fb7bdae493c5717041101
Parami <paramiashinsana@gmail.com>
2024-07-02 16:19:28 +0530

    Update TicketService

--- TicketService/src/main/java/org/example2/ticketservice/service/TicketService.java
+++ TicketService/src/main/java/org/example2/ticketservice/service/TicketService.java
@@ -1,4 +1,13 @@
 package org.example2.ticketservice.service;
 
+import org.example2.ticketservice.dto.TicketDTO;
+
+import java.util.List;
+
 public interface TicketService {
+    TicketDTO issueTicketAtEntrance(TicketDTO ticketDTO);
+    void deleteTicket(String id);
+    void issueTicketAtExit(String id, TicketDTO ticketDTO);
+    List<TicketDTO> getAllTicketDetails();
+    TicketDTO getSelectedTicketDetails(String id);
 }
