eba619c7c68ccede8a3ebf3095f51a5aa50d03df
Parami <paramiashinsana@gmail.com>
2024-07-02 23:44:56 +0530

    Update PaymentMapping

--- PaymentService/src/main/java/org/example2/paymentservice/util/PaymentMapping.java
+++ PaymentService/src/main/java/org/example2/paymentservice/util/PaymentMapping.java
@@ -1,9 +1,25 @@
 package org.example2.paymentservice.util;
 
 import lombok.RequiredArgsConstructor;
+import org.example2.paymentservice.dto.PaymentDTO;
+import org.example2.paymentservice.entity.PaymentEntity;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class PaymentMapping {
+    private final ModelMapper modelMapper;
+
+    public PaymentDTO toPaymentDTO(PaymentEntity paymentEntity) {
+        return  modelMapper.map(paymentEntity, PaymentDTO.class);
+    }
+    public PaymentEntity toPayment(PaymentDTO paymentDTO) {
+        return  modelMapper.map(paymentDTO, PaymentEntity.class);
+    }
+    public List<PaymentDTO> toPaymentDTOList(List<PaymentEntity> paymentEntities) {
+        return modelMapper.map(paymentEntities, List.class);
+    }
 }
