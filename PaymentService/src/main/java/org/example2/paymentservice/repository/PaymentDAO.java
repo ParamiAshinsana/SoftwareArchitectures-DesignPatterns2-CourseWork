8ff88b8aa40de8ec355a04fe1fc8328b6f8e1ee1
Parami <paramiashinsana@gmail.com>
2024-07-02 23:42:19 +0530

    Update PaymentDAO

--- PaymentService/src/main/java/org/example2/paymentservice/repository/PaymentDAO.java
+++ PaymentService/src/main/java/org/example2/paymentservice/repository/PaymentDAO.java
@@ -1,7 +1,9 @@
 package org.example2.paymentservice.repository;
 
+import org.example2.paymentservice.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentDAO {
+public interface PaymentDAO extends JpaRepository<PaymentEntity, String> {
 }
