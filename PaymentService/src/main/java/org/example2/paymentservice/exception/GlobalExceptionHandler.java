a8526521e72d29c32563894894bc86ca392cc907
Parami <paramiashinsana@gmail.com>
2024-07-03 15:07:56 +0530

    Update GlobalExceptionHandler

--- PaymentService/src/main/java/org/example2/paymentservice/exception/GlobalExceptionHandler.java
+++ PaymentService/src/main/java/org/example2/paymentservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,22 @@
 package org.example2.paymentservice.exception;
 
-public class GlobalExceptionHandler
-{
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidPaymentException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidPaymentException(InvalidPaymentException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+
 }
