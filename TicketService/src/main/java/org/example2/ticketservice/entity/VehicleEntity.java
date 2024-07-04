92f70920e31bf1b43cc2915c2af7b73008d34be8
Parami <paramiashinsana@gmail.com>
2024-07-04 21:49:42 +0530

    Add VehicleEntity in TicketService

---
+++ TicketService/src/main/java/org/example2/ticketservice/entity/VehicleEntity.java
@@ -0,0 +1,24 @@
+package org.example2.ticketservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+//@Table(name = "vehicle")
+@Table(name = "vehicle", uniqueConstraints = {@UniqueConstraint(columnNames = {"vehicleType", "vehicleNo"})})
+public class VehicleEntity {
+    @Id
+    private String vehicleRegistrationId;
+    private String vehicleType;
+    private String fuelType;
+    @Column(unique = false)
+    private String vehicleNo;
+    private String nameOfOwner;
+    private String addressOfOwner;
+    private String registeredDate;
+}
